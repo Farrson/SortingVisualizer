f4608d18168d1ffb1ddf342df53349ccfb90cd7b
Farrson <71981554+Farrson@users.noreply.github.com>
2021-02-11 19:27:54 +0100

    Add files via upload
    removed some code leftovers

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-import sorting
 import sys
 from PyQt5 import QtCore, QtWidgets
 from ui.mainwindow import Ui_MainWindow
 
 
 if __name__ == '__main__':
-    # create array
-    array = sorting.get_random_array(50)
-    # use GUI
     app = QtWidgets.QApplication(sys.argv)
     window = QtWidgets.QMainWindow()
-    window.setWindowTitle("Sorting Visualizer")
-
     ui_window = Ui_MainWindow()
     ui_window.setupUi(window)
-
     window.show()
     sys.exit(app.exec_())
